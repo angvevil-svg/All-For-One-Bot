f067f3481c7f7a917d51ffaf33a6d8259732c0c9
angvevil-svg <angvevil@gmail.com>
2026-02-28 08:45:37 +0530

    Refactor database connection handling and imports

--- src/handlers/1-database.js
+++ src/handlers/1-database.js
@@ -1,72 +1,89 @@
-const {
-  QuickDB
-} = require("quick.db");
+const { QuickDB, MongoDriver } = require("quick.db");
 const clc = require("cli-color");
 const error = require("../functions/error");
 const post = require("../functions/post");
-const database = (require("../../config")).source.database;
-const { QuickDB } = require("quick.db");
-const { SQLiteDriver } = require("quick.db");
 
-const driver = new SQLiteDriver();
-const db = new QuickDB({ driver });
+const database = require("../../config").source.database;
 
-module.exports = db;
 /**
- * 
- * @param {import("discord.js").Client} client 
- * @returns {void}
+ * @param {import("discord.js").Client} client
  */
 module.exports = async (client) => {
+
   try {
+
+    let driver;
+
     switch (database.type) {
-      case "sql": {
-        const {
-          SqliteDriver
-        } = require("quick.db");
-        driver = new SqliteDriver();
-      } break;
-
-      case "mysql": {
-        const {
-          MySQLDriver
-        } = require("quick.db");
-        driver = new MySQLDriver(database.mysql)
-      } break;
-
-      case "json": {
-        const {
-          JSONDriver
-        } = require("quick.db");
-        driver = new JSONDriver();
-      } break;
 
       case "mongodb": {
-        const {
-          MongoDriver
-        } = require("quickmongo");
-        driver = new MongoDriver(database.mongoURL);
-        await driver.connect();
-      } break;
-    };
 
-    const db = new QuickDB({
-      driver
-    });
+        driver = new MongoDriver(database.mongoURL);
+
+        await driver.connect();
+
+        break;
+
+      }
+
+      case "sql": {
+
+        const { SQLiteDriver } = require("quick.db");
+
+        driver = new SQLiteDriver();
+
+        break;
+
+      }
+
+      case "json": {
+
+        const { JSONDriver } = require("quick.db");
+
+        driver = new JSONDriver();
+
+        break;
+
+      }
+
+      case "mysql": {
+
+        const { MySQLDriver } = require("quick.db");
+
+        driver = new MySQLDriver(database.mysql);
+
+        break;
+
+      }
+
+      default:
+
+        throw new Error("Invalid database type");
+
+    }
+
+    const db = new QuickDB({ driver });
+
     await db.init();
+
     client.db = db;
-    post(`Database Is Successfully Connected!! (Type: ${database.type.toLocaleUpperCase()})`, "S")
+
+    post(
+      `Database Is Successfully Connected!! (Type: ${database.type.toUpperCase()})`,
+      "S"
+    );
+
   } catch (e) {
-    post(`${clc.red(`Database Doesn't Connected!! (Type: ${database.type.toLocaleUpperCase()})`)}`, "E", "red", "redBright")
-    error(e)
+
+    post(
+      clc.red(
+        `Database Doesn't Connected!! (Type: ${database.type.toUpperCase()})`
+      ),
+      "E"
+    );
+
+    error(e);
+
   }
-}
-/**
- * @copyright
- * Coded by Sobhan-SRZA (mr.sinre) | https://github.com/Sobhan-SRZA
- * @copyright
- * Work for Persian Caesar | https://dsc.gg/persian-caesar
- * @copyright
- * Please Mention Us "Persian Caesar", When Have Problem With Using This Code!
- * @copyright
-*/
+
+};
